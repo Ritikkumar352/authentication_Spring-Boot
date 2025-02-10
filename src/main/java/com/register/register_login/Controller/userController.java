d426c699d92d38bb2ecb1dc55eed7d2a7faca03b
ritikkumar352 <krritik2002@gmail.com>
2025-02-11 00:31:23 +0530

    DB connection and user mobel (entity)

--- src/main/java/com/register/register_login/Controller/userController.java
+++ src/main/java/com/register/register_login/Controller/userController.java
@@ -1,4 +1,31 @@
 package com.register.register_login.Controller;
 
-public class userCOntroller {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class userController {
+    @GetMapping("/")
+    @ResponseBody
+    public String home() {
+        return "home";
+    }
+
+    @PostMapping("/login")
+    public void login(){
+
+    }
+    @PostMapping("/register")
+    public void register(){
+
+    }
+
+    @GetMapping("/about")
+    @ResponseBody
+    public String about(){
+        return "about";
+    }
+
 }
--- src/main/java/com/register/register_login/securityConfig/securityConfig.java
+++ src/main/java/com/register/register_login/securityConfig/securityConfig.java
@@ -1,32 +1,9 @@
 package com.register.register_login.securityConfig;
 
+import org.springframework.context.annotation.Configuration;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+@Configuration
+public class securityConfig{
 
-@Controller
-public class securityConfig {
-    @GetMapping("/")
-    @ResponseBody
-    public String home() {
-        return "home";
-    }
-
-    @PostMapping("/login")
-    public void login(){
-
-    }
-    @PostMapping("/register")
-    public void register(){
-
-    }
-
-    @GetMapping("/about")
-    @ResponseBody
-    public String about(){
-        return "about";
-    }
 
 }
