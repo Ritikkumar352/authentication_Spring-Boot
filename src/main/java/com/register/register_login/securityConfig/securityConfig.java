e2a96a4bd6d673be7578bdcec1fbaed6aa29c9aa
ritikkumar352 <krritik2002@gmail.com>
2025-02-10 23:59:28 +0530

    controller and config filr

--- src/main/java/com/register/register_login/securityConfig/securityConfig.java
+++ src/main/java/com/register/register_login/securityConfig/securityConfig.java
@@ -1,4 +1,32 @@
 package com.register.register_login.securityConfig;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class securityConfig {
+    @GetMapping("/")
+    @ResponseBody
+    public String home() {
+        return "home";
+    }
+
+    @PostMapping("/login")
+    public void login(){
+
+    }
+    @PostMapping("/register")
+    public void register(){
+
+    }
+
+    @GetMapping("/about")
+    @ResponseBody
+    public String about(){
+        return "about";
+    }
+
 }
